051c960977279033b3801bd48b763243ed0e26ba
vishnuu5 <vishnuathmakuru20@gmail.com>
2025-01-05 12:39:05 +0530

    update files

--- src/Pages/Home/MyPortfolio.jsx
+++ src/Pages/Home/MyPortfolio.jsx
@@ -41,7 +41,7 @@ export default function MyPortfolio() {
                 <p className="text-md">{item.description}</p>
               </div>
               <p className="text-sm portfolio--link">
-                {item.gitlink}
+                <a href={item.gitlink}>View code in github</a>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
